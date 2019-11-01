0f1c6e91430ec5a93db1be16b6b1f2c0f23e0b89
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2019-10-31 22:03:10 -0700

    Tweak figure, and minor edits.

--- images/ricker.r
+++ images/ricker.r
@@ -5,12 +5,14 @@ ricker <- function(Svec, alpha, beta)
 }
 
 S = seq(0, 10, by=0.01)
-png("ricker.png")
+png("ricker.png",
+    width = 700)
+par(cex = 2,
+    mai = c(1.7, 1.7, 0.2, 1))
 plot(S, ricker(Svec = S, alpha = 2, beta = 1),
      type = "l",
      xlab = "Spawners, S",
-     ylab = "Recruits, R",
-     cex = 3)
+     ylab = "Recruits, R")
 lines(S, ricker(Svec = S, alpha = 1, beta = 1),
       col = "red")
 lines(S, ricker(Svec = S, alpha = 2, beta = 2),
